088066527dfb71b1733595b1a8d9d7985654aff4
Sam Peterson <samuel.e.peterson@gmail.com>
2017-06-30 13:29:50 -0400

    began moving auth to main component

--- src/app/Main/Main.jsx
+++ src/app/Main/Main.jsx
@@ -1,16 +1,28 @@
-import React             from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Switch, Route }    from 'react-router-dom';
 
-import Header            from '../Header/Header';
-import { Dashboard }     from '../../pages';
+import Header               from '../Header/Header';
+import { Dashboard }        from '../../pages';
 
-const Main = () => (
-  <div>
-    <Header />
-    <Switch>
-      <Route path="/dashboard" component={Dashboard} />
-    </Switch>
-  </div>
-);
+class Main extends Component {
+  componentDidMount() {
+  }
 
-export default Main;
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const { user } = state;
+  return { ...ownProps, user };
+};
+
+export default connect(mapStateToProps)(Main);
