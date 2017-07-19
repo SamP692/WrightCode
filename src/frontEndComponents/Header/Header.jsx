2083759128da7bb58e58a44c20fc7a9ce43a718a
Sam Peterson <samuel.e.peterson@gmail.com>
2017-07-19 09:11:06 -0400

    complete successful nav toggle action dispatch from header component

--- src/frontEndComponents/Header/Header.jsx
+++ src/frontEndComponents/Header/Header.jsx
@@ -13,32 +13,54 @@ class Header extends Component {
     this.signOut = this.signOut.bind(this);
   }
 
+  componentDidMount() {
+    console.log(this.props);
+  }
+
   signOut() {
     authService.endSession();
     this.props.dispatch({ type: 'END_SESSION' });
     this.props.dispatch(push('/'));
   }
 
+  toggleNav(navItem) {
+    const payload = { navSelected: navItem };
+    this.props.dispatch({ type: 'TOGGLE_NAV', payload });
+  }
+
   render() {
+    const recordEvent = (event) => {
+      console.log('%cEvent Fired: ', 'color: blue; font-weight: bold', event);
+    };
+
     return (
       <div id="headerContainer">
-        <Link to="/dashboard">
+        <Link onClick={() => this.toggleNav('dashboard')} to="/dashboard">
           Dashboard
         </Link>
-        <button onMouseOver={() => this.recordEvent('Over')} onBlur={() => this.recordEvent('Blur')}>
+        <button
+          onClick={() => this.toggleNav('projects')}
+          onMouseOver={() => recordEvent('Over')}
+          onBlur={() => recordEvent('Blur')}
+        >
           Projects
         </button>
         <button onClick={this.signOut}>
           Sign Out
         </button>
+        <div>{this.props.headerUi.navSelected}</div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const { user } = state;
-  return { ...ownProps, user };
+  const { user, headerUi } = state;
+  return {
+    ...ownProps,
+    user,
+    headerUi,
+  };
 };
 
 export default connect(mapStateToProps)(Header);
